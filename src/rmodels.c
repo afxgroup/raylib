b0b6468aa50aefb154c5d8529d53b4f78d031519
Andrea Palmatè <info@papersoft.it>
2025-02-06 21:14:40 +0100

    Some more endian fixes for gltf files

--- src/rmodels.c
+++ src/rmodels.c
@@ -5238,6 +5238,7 @@ static Model LoadGLTF(const char *fileName)
     // Macro to simplify attributes loading code
     #define LOAD_ATTRIBUTE(accesor, numComp, srcType, dstPtr) LOAD_ATTRIBUTE_CAST(accesor, numComp, srcType, dstPtr, srcType)
 
+#ifndef __amigaos4__
     #define LOAD_ATTRIBUTE_CAST(accesor, numComp, srcType, dstPtr, dstType) \
     { \
         int n = 0; \
@@ -5251,7 +5252,37 @@ static Model LoadGLTF(const char *fileName)
             n += (int)(accesor->stride/sizeof(srcType));\
         }\
     }
-
+#else
+ #define LOAD_ATTRIBUTE_CAST(accesor, numComp, srcType, dstPtr, dstType) \
+    { \
+        int n = 0; \
+        srcType *buffer = (srcType *)accesor->buffer_view->buffer->data + accesor->buffer_view->offset/sizeof(srcType) + accesor->offset/sizeof(srcType); \
+        char *dst_type_str = #dstType; \
+        bool is_dst_float = false;\
+        bool is_dst_ushort = false;\
+        if (strcmp(dst_type_str, "float") == 0 ) {is_dst_float = true;} \
+        if (strcmp(dst_type_str, "unsigned short") == 0 ) {is_dst_ushort = true;} \
+        for (unsigned int k = 0; k < accesor->count; k++) \
+        {\
+            for (int l = 0; l < numComp; l++) \
+            {\
+                dstPtr[numComp*k + l] = (dstType)buffer[n + l];\
+                if (is_dst_float)\
+                {\
+                    union { uint32_t u; float f ;} x ;\
+                    x.f = dstPtr[numComp*k + l];\
+                    x.u = __builtin_bswap32(x.u);\
+                    dstPtr[numComp*k + l] = x.f;\
+                }\
+                if (is_dst_ushort)\
+                {\
+                    dstPtr[numComp*k + l] = __builtin_bswap16(dstPtr[numComp*k + l]);\
+                }\
+            }\
+            n += (int)(accesor->stride/sizeof(srcType));\
+        }\
+    }
+#endif
     Model model = { 0 };
 
     // glTF file loading
